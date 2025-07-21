154639174177432a741b0c870ccbbc7a6c0c019d
rancorzinho <9276791+rancorzinho@users.noreply.github.com>
2025-07-21 14:38:47 -0300

    add declaredSecrets

--- helpers/github/actions.d.ts
+++
@@ -1,2 +0,0 @@
-import { IDefaultKeyPair } from '../../components/types/workflows/common';
-export declare function checkRequiredArgs(uses: string, withArgs: IDefaultKeyPair): Promise<boolean | undefined>;
--- index.d.ts
+++
@@ -1,4 +0,0 @@
-import { Job } from './components/workflows/job';
-import { Step } from './components/workflows/step';
-import { Workflow } from './components/workflows/workflow';
-export { Job, Step, Workflow };
